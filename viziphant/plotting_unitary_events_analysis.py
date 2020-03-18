9243b49577c87dcc9d827b18b040569d1844c4d9
Maximilian Kramer <m.kramer@fz-juelich.de>
2020-03-18 16:08:30 +0100

    added crucial key to the params_dict;; changed some formats (remove blank lines and blanks in function call);; x-axis label now, just at the last subplot (unitary events)

--- viziphant/plotting_unitary_events_analysis.py
+++ viziphant/plotting_unitary_events_analysis.py
@@ -1,8 +1,7 @@
-import os
 import math
-import numpy as np
-import quantities as pq
+
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter)
 
 import elephant.unitary_event_analysis as ue
@@ -19,12 +18,12 @@ params_dict_default = {
     'hspace': 1,
     # width white space between subplots
     'wspace': 0.5,
-    # orientation
+    # orientation (figure margins)
     'top': 0.9,
     'bottom': 0.05,
     'right': 0.95,
     'left': 0.1,
-    # font size         #Schriftgroesse
+    # font size
     'fsize': 12,
     # the actual unit ids from the experimental recording
     'unit_real_ids': [1, 2],
@@ -34,25 +33,39 @@ params_dict_default = {
     'S_ylim': (-3, 3),
     # size of the marker
     'marker_size': 5,
-    # major tick width on the time scale
+    # major tick width on the time scale (x-axis)
     'major_tick_width_time': 200,
-    # number n of minor ticks between major ones on the time scale:
+    # number n of minor ticks between major ones on the time scale (x-axis)
     'number_minor_ticks_time': 1,
+    # size of the y-ticks intervall for rasterplots
+    'y_tick_interval': 15,
+    # show figure
+    'showfig': True,
+    # save figure
+    'savefig': False,
+    # path, file name and format for saving the figure
+    'path_filname_format': 'figure.pdf'
 }
 
-target_images_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)),
-                                 "viziphant/tests/target_images")
-PLOT_UE_TARGET_PATH = os.path.join(
-    target_images_dir, "target_plot_ue.png")
 
-def plot_UE(
-        data, joint_suprise_dict, significance_level, binsize,
-        window_size, window_step, n_neurons, **plot_params_user):
+def plot_UE(data, joint_suprise_dict, significance_level, binsize,
+            window_size, window_step, **plot_params_user):
+
+    # # set common variables
+    n_neurons = len(data[0])
+    t_start = data[0][0].t_start
+    t_stop = data[0][0].t_stop
+    t_winpos = ue._winpos(t_start, t_stop, window_size, window_step)
+    n_trail = len(data)
+    joint_suprise_significance = ue.jointJ(significance_level)
+    xlim_left = (min(t_winpos)).rescale('ms').magnitude
+    xlim_right = (max(t_winpos) + window_size).rescale('ms').magnitude
 
     # update params_dict_default with user input
     params_dict = params_dict_default.copy()
     params_dict.update(plot_params_user)
 
+    # TODO: maybe remove this checking user-entries part to a separate function
     if len(params_dict['unit_real_ids']) != n_neurons:
         raise ValueError(
             'length of unit_ids should be equal to number of neurons! \n'
@@ -63,23 +76,13 @@ def plot_UE(
     plt.subplots_adjust(hspace=params_dict['hspace'],
                         wspace=params_dict['wspace'])
 
-    # # set common variables
-    t_start = data[0][0].t_start
-    t_stop = data[0][0].t_stop
-    t_winpos = ue._winpos(t_start, t_stop, window_size, window_step)
-    n_trail = len(data)
-    joint_suprise_significance = ue.jointJ(significance_level)
-
-    xlim_left = (min(t_winpos)).rescale('ms').magnitude
-    xlim_right = (max(t_winpos) + window_size).rescale('ms').magnitude
-
     # set y-axis for raster plots with ticks and labels
-    y_tick_interval = 15
+    y_tick_interval = params_dict['y_tick_interval']
     y_ticks_list = []
     # find start y-tick position for each trail
     for yt1 in range(1, n_neurons * n_trail, n_trail + 1):
         y_ticks_list.append(yt1)
-    # find in-between y-tick position for each trail with interval of 15
+    # find in-between y-tick position for each trail with the specific interval
     for n in range(n_neurons):
         for yt2 in range(n * (n_trail + 1) + y_tick_interval,
                          (n + 1) * n_trail, y_tick_interval):
@@ -109,8 +112,9 @@ def plot_UE(
             for e_val in params_dict['events'][key]:
                 axes_name.axvline(e_val, ls='-', lw=params_dict['lw'],
                                   color='r')
-                axes_name.text(e_val, params_dict['S_ylim'][0],
-                               key, fontsize=params_dict['fsize'], color='r')
+                if axes_name.get_geometry()[2] == 6:
+                    axes_name.text(x=e_val-10, y=-65, s=key, fontsize=9,
+                                   color='r')
 
     print('plotting Unitary Event Analysis ...')
 
@@ -119,32 +123,25 @@ def plot_UE(
     axes1.set_title('Spike Events')
     for n in range(n_neurons):
         for trial, data_trial in enumerate(data):
-            axes1.plot(
-                data_trial[n].rescale('ms').magnitude,
-                np.ones_like(data_trial[n].magnitude) * trial +
-                n * (n_trail + 1) + 1, ls='none', marker='.', color='k',
-                markersize=0.5)
+            axes1.plot(data_trial[n].rescale('ms').magnitude,
+                       np.ones_like(data_trial[n].magnitude) * trial +
+                       n * (n_trail + 1) + 1, ls='none', marker='.',
+                       color='k', markersize=0.5)
         if n < n_neurons - 1:
             axes1.axhline((trial + 2) * (n + 1), lw=params_dict['lw'],
                           color='b')
-
     axes1.set_xlim(xlim_left, xlim_right)
     axes1.set_ylim(0, (trial + 2) * (n + 1) + 1)
-
     set_xticks(axes1)
-
     axes1.set_yticks(y_ticks_list)
     axes1.set_yticklabels(y_ticks_labels_list, fontsize=params_dict['fsize'])
-
     for n in range(n_neurons):
         axes1.text(xlim_right + 20, n * (n_trail + 1),
                    f"Unit {params_dict['unit_real_ids'][n]}")
-
-    axes1.set_xlabel('Time [ms]', fontsize=params_dict['fsize'])
     axes1.set_ylabel('Trial', fontsize=params_dict['fsize'])
 
     print('plotting Spike Rates ...')
-    axes2 = plt.subplot(6, 1, 2)
+    axes2 = plt.subplot(6, 1, 2, sharex=axes1)
     axes2.set_title('Spike Rates')
     # psth = peristimulu time histogram
     max_val_psth = 0
@@ -154,25 +151,19 @@ def plot_UE(
             joint_suprise_dict['rate_avg'][:, n].rescale('Hz'),
             label=f"Unit {params_dict['unit_real_ids'][n]}",
             lw=params_dict['lw'])
-        if max(joint_suprise_dict['rate_avg'][:, n]) > \
-                max_val_psth:
+        if max(joint_suprise_dict['rate_avg'][:, n]) > max_val_psth:
             max_val_psth = max(joint_suprise_dict['rate_avg'][:, n])
-
     axes2.set_xlim(xlim_left, xlim_right)
-
     max_val_psth = max_val_psth.rescale('Hz').magnitude
-
     axes2.set_ylim(0, max_val_psth + max_val_psth/10)
-
     set_xticks(axes2)
     axes2.set_yticks([0, int(max_val_psth / 2), int(max_val_psth)])
     mark_epochs(axes2)
     axes2.legend(bbox_to_anchor=(1, 1), fancybox=True, shadow=True)
-    axes2.set_xlabel('Time [ms]', fontsize=params_dict['fsize'])
     axes2.set_ylabel('(1/s)', fontsize=params_dict['fsize'])
 
     print('plotting Coincidence Events ...')
-    axes3 = plt.subplot(6, 1, 3)
+    axes3 = plt.subplot(6, 1, 3, sharex=axes1)
     axes3.set_title('Coincidence Events')
     for n in range(n_neurons):
         for trial, data_trial in enumerate(data):
@@ -193,45 +184,36 @@ def plot_UE(
                           color='b')
     axes3.set_xlim(xlim_left, xlim_right)
     axes3.set_ylim(0, (trial + 2) * (n + 1) + 1)
-
     set_xticks(axes3)
     axes3.set_yticks(y_ticks_list)
     axes3.set_yticklabels(y_ticks_labels_list, fontsize=params_dict['fsize'])
     mark_epochs(axes3)
-    axes3.set_xlabel('Time [ms]', fontsize=params_dict['fsize'])
     axes3.set_ylabel('Trial', fontsize=params_dict['fsize'])
 
     print('plotting Coincidence Rates ..')
-    axes4 = plt.subplot(6, 1, 4)
+    axes4 = plt.subplot(6, 1, 4, sharex=axes1)
     axes4.set_title('Coincidence Rates')
-    axes4.plot(
-        t_winpos + window_size / 2., joint_suprise_dict['n_emp'] / (
-                     window_size.rescale('s').magnitude * n_trail),
-        label='empirical', lw=params_dict['lw'], color='c')
-    axes4.plot(
-        t_winpos + window_size / 2., joint_suprise_dict['n_exp'] / (
-                     window_size.rescale('s').magnitude * n_trail),
-        label='expected', lw=params_dict['lw'], color='m')
-
+    axes4.plot(t_winpos + window_size / 2., joint_suprise_dict['n_emp'] /
+               (window_size.rescale('s').magnitude * n_trail),
+               label='empirical', lw=params_dict['lw'], color='c')
+    axes4.plot(t_winpos + window_size / 2., joint_suprise_dict['n_exp'] /
+               (window_size.rescale('s').magnitude * n_trail),
+               label='expected', lw=params_dict['lw'], color='m')
     axes4.set_xlim(xlim_left, xlim_right)
     set_xticks(axes4)
-
     y_ticks = axes4.get_ylim()
     axes4.set_yticks([0, y_ticks[1] / 2, y_ticks[1]])
     mark_epochs(axes4)
     axes4.legend(bbox_to_anchor=(1, 1), fancybox=True, shadow=True)
-    axes4.set_xlabel('Time [ms]', fontsize=params_dict['fsize'])
     axes4.set_ylabel('(1/s)', fontsize=params_dict['fsize'])
 
     print('plotting Statistical Significance ...')
-    axes5 = plt.subplot(6, 1, 5)
+    axes5 = plt.subplot(6, 1, 5, sharex=axes1)
     axes5.set_title('Statistical Significance')
-    axes5.plot(
-        t_winpos + window_size / 2., joint_suprise_dict['Js'],
-        lw=params_dict['lw'], color='k')
+    axes5.plot(t_winpos + window_size / 2., joint_suprise_dict['Js'],
+               lw=params_dict['lw'], color='k')
     axes5.set_xlim(xlim_left, xlim_right)
     axes5.set_ylim(params_dict['S_ylim'])
-
     axes5.axhline(joint_suprise_significance, ls='-', color='r')
     axes5.axhline(-joint_suprise_significance, ls='-', color='b')
     axes5.text(t_winpos[30], joint_suprise_significance + 0.3, '$\\alpha +$',
@@ -241,52 +223,40 @@ def plot_UE(
     set_xticks(axes5)
     axes5.set_yticks([ue.jointJ(0.99), ue.jointJ(0.5), ue.jointJ(0.01)])
     mark_epochs(axes5)
-    axes5.set_xlabel('Time [ms]', fontsize=params_dict['fsize'])
     axes5.set_yticklabels([0.99, 0.5, 0.01])
 
     print('plottting Unitary Events ...')
-    axes6 = plt.subplot(6, 1, 6)
+    axes6 = plt.subplot(6, 1, 6, sharex=axes1)
     axes6.set_title('Unitary Events')
+    # sig_inx_win: indices of significant JointSurprises within a window
+    # x: indices of coincidence events in analysis window of a trail
+    # xx: indices_of_unitary_events_in_analysis_window_of_a_trial
     for n in range(n_neurons):
         for trial, data_trial in enumerate(data):
-            axes6.plot(
-                data_trial[n].rescale('ms').magnitude,
-                np.ones_like(data_trial[n].magnitude) * trial +
-                n * (n_trail + 1) + 1, ls='None', marker='.',
-                markersize=0.5, color='k')
-            # TODO: rename sig_idx_win to
-            #  indices_of_significant_JointSurprises_within_a_window?
-
+            axes6.plot(data_trial[n].rescale('ms').magnitude,
+                       np.ones_like(data_trial[n].magnitude) * trial +
+                       n * (n_trail + 1) + 1, ls='None', marker='.',
+                       markersize=0.5, color='k')
             sig_idx_win = np.where(
                 joint_suprise_dict['Js'] >= joint_suprise_significance)[0]
             if len(sig_idx_win) > 0:
-                # TODO: rename x and xx to be self-explaining
-                #  x: indices_of_coincidence_events_in_analysis_window_of_a_trail
-                #  xx: indices_of_unitary_events_in_analysis_window_of_a_trial
                 x = np.unique(
                     joint_suprise_dict['indices']['trial' + str(trial)])
                 if len(x) > 0:
                     xx = []
                     for j in sig_idx_win:
-                        # TODO: remove np.nonzero or np.where and use
-                        #  mask instead -> x[condition] -> returns indices of
-                        #  elements in x where the condition is true
-                        xx = np.append(xx, x[np.where(
-                            (x * binsize >= t_winpos[j]) &
-                            (x * binsize < t_winpos[j] + window_size))])
-                    axes6.plot(
-                        np.unique(xx) * binsize,
-                        np.ones_like(np.unique(xx)) * trial + n * (n_trail + 1)
-                        + 1, markersize=params_dict['marker_size'],
-                        marker='s', ls='', markerfacecolor='none',
-                        markeredgecolor='r')
+                        xx = np.append(xx, x[(x * binsize >= t_winpos[j]) &
+                            (x * binsize < t_winpos[j] + window_size)])
+                    axes6.plot(np.unique(xx) * binsize, np.ones_like(
+                               np.unique(xx)) * trial + n * (n_trail + 1) + 1,
+                               markersize=params_dict['marker_size'],
+                               marker='s', ls='', markerfacecolor='none',
+                               markeredgecolor='r')
         if n < n_neurons - 1:
             axes6.axhline((trial + 2) * (n + 1), lw=params_dict['lw'],
                           color='b')
-
     axes6.set_xlim(xlim_left, xlim_right)
     axes6.set_ylim(0, (trial + 2) * (n + 1) + 1)
-
     set_xticks(axes6)
     axes6.set_yticks(y_ticks_list)
     axes6.set_yticklabels(y_ticks_labels_list, fontsize=params_dict['fsize'])
@@ -294,5 +264,11 @@ def plot_UE(
     axes6.set_xlabel('Time [ms]', fontsize=params_dict['fsize'])
     axes6.set_ylabel('Trial', fontsize=params_dict['fsize'])
 
-    plt.savefig(PLOT_UE_TARGET_PATH)
-    plt.close()
+    if params_dict['savefig']:
+        plt.savefig(params_dict['path_filename_format'])
+        if not params_dict['showfig']:
+            plt.cla()
+            plt.close()
+    if params_dict['showfig']:
+        plt.show()
+        plt.close()
