5cf15e9f20a0a9787963e96b5f35bbf12ae7ebe2
Maximilian Kramer <m.kramer@fz-juelich.de>
2020-04-15 16:40:17 +0200

    added/changed the suggestions from pull-request 'unitary event analysis plot #7' and addepted the changes of pull-ruquest 'Return Axes instead of plotting and/or saving the figure. #14'(old viziphant-repo)

--- viziphant/tests/test_plotUE.py
+++ viziphant/tests/test_plotUE.py
@@ -1,13 +1,13 @@
-import os
 import unittest
 
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 import neo
 import numpy as np
 import quantities as pq
 
 import elephant.unitary_event_analysis as ue
-from viziphant.plotting_unitary_events_analysis import plot_UE
+from viziphant.plotting_unitary_events_analysis import plot_unitary_events
 
 
 def compare_images(path_target_image, path_result_image):
@@ -36,18 +36,12 @@ class UETestCase(unittest.TestCase):
             winstep=10 * pq.ms, pattern_hash=[3])
 
     def test_plot_UE(self):
-        target_images_dir = os.path.join(
-            os.path.dirname(os.path.dirname(__file__)),
-            "tests/target_images")
-        PLOT_UE_TARGET_PATH = os.path.join(
-            target_images_dir, "target_plot_ue.png")
-        plot_UE(
+        result = plot_unitary_events(
             data=self.spiketrains, joint_surprise_dict=self.UE,
             significance_level=0.05, binsize=5*pq.ms,
             window_size=100*pq.ms, window_step=10*pq.ms, n_neurons=2,
-            **{'events': {'Vision': [1000]*pq.ms, 'Action': [1500]*pq.ms},
-               'savefig': True, 'showfig': False,
-               'path_filename_format': PLOT_UE_TARGET_PATH})
+            **{'events': {'Vision': [1000]*pq.ms, 'Action': [1500]*pq.ms}})
+        plt.show(result)
 
 
 if __name__ == '__main__':
